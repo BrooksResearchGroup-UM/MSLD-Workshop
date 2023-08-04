f08c2c4f0f6dc6864e9f81394c7e97e0c00ccde2
Ryan Lee Hayes <rhayes1@login-i17.local>
2023-08-04 00:53:32 -0700

    Cleaning old stuff out of mutations tutorial

--- 6ProteinMutation/protein_mutation_setup/mdsetup/SubSetup.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-sbatch --time=240 --ntasks=1 --tasks-per-node=1 --cpus-per-task=1 -p free-gpu -A rhayes1_lab_gpu --gres=gpu:1 --export=ALL --output=`pwd`/outrun --error=`pwd`/errrun ./RunSetup.sh
