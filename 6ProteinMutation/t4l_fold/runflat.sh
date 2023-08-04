c7214f7bcbcbae29de68f7f3e0af9998fa41df63
Ryan Lee Hayes <rhayes1@login-i17.local>
2023-08-04 03:15:48 -0700

    Final mutation commit of the night

--- 6ProteinMutation/t4l_fold/runflat.sh
+++ 6ProteinMutation/t4l_fold/runflat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -c "import alf; alf.initialize(engine='pycharmm')"
-python -c "import alf; alf.runflat(1,100,13000,39000,engine='pycharmm')"
-python -c "import alf; alf.runflat(101,110,125000,375000,engine='pycharmm')"
+python -c "import alf; alf.initialize(engine='bladelib')"
+python -c "import alf; alf.runflat(1,100,13000,39000,engine='bladelib')"
+python -c "import alf; alf.runflat(101,110,125000,375000,engine='bladelib')"
